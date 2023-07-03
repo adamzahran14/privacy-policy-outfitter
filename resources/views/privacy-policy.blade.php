2132fe75c0fe715bd5369b94c40a03f37c965a95
adamzahran14 <adamzahran72@gmail.com>
2023-07-03 16:36:27 +0700

    Finishing

---
+++ resources/views/privacy-policy.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy Policy</title>
+    <link rel="icon" href="{{ url('images/logo.png') }}">
+</head>
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Noto+Sans:wght@400;700&display=swap");
+
+    :root {
+        --dark-1: #101d2e;
+        --dark-2: #0f1c2d;
+        --dark-3: #1e2a39;
+        --grey-1: #6f7781;
+        --grey-2: #626a7f;
+        --black: #000000;
+        --noto-sans: 'Noto Sans', sans-serif;
+        --montserrat: 'Montserrat', sans-serif;
+        --poppins: 'Poppins', sans-serif;
+        ;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        background-image: url('images/bg-logo.png');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    body::after {
+        background: white;
+        content: '';
+        position: absolute;
+        inset: 0;
+        opacity: .3;
+        z-index: -1;
+    }
+
+    .container {
+        padding: 30px;
+    }
+
+    .privacy-box {
+        max-width: 600px;
+        background-color: white;
+        border-radius: 20px;
+        color: black;
+        font-family: var(--montserrat);
+        padding: 30px 30px;
+    }
+
+    .privacy-text {
+        padding: 0 20px;
+        height: 400px;
+        overflow-y: auto;
+        font-size: 14px;
+        font-weight: 500;
+        color: black;
+    }
+
+    .privacy-text::-webkit-scrollbar {
+        width: 2px;
+        background-color: grey;
+    }
+
+    .privacy-text::-webkit-scrollbar-thumb {
+        background-color: black;
+    }
+
+    .privacy-text h2 {
+        text-transform: uppercase;
+    }
+
+    .title {
+        margin-top: 0;
+    }
+
+    .privacy-box h3 {
+        position: relative;
+        bottom: 16px;
+        text-align: center;
+        padding: 0 40px;
+    }
+
+    .privacy-box h4 {
+        position: relative;
+        top: 16px;
+        font-size: 14px;
+        font-weight: 500;
+        text-align: center;
+        padding: 0 40px;
+    }
+
+    @media screen and (max-width: 676px) {
+        /* body {
+            margin: 36px;
+        } */
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="privacy-box">
+            <h3>OUTFITTER AR FASHION TRY ON</h3>
+            <div class="privacy-text">
+                <h2 class="title">Privacy Policy</h2>
+
+                <p>Last updated : 2023-07-02</p>
+
+                <p>D3IF COOL built the Outfitter app as a Free app. This SERVICE is provided by D3IF COOL at no cost and is intended for use as is.
+                    This page is used to inform visitors regarding our policies with the collection, use, 
+                    and disclosure of Personal Information if anyone decided to use our Service.</p>
+
+                <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. 
+                    The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
+                    The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, 
+                    which are accessible at Outfitter unless otherwise defined in this Privacy Policy.</p>
+
+                <h2>Information Collection and Use</h2>
+                <p>For a better experience, while using our Service, we may require you to provide us with certain 
+                    personally identifiable information, including but not limited to Outfitter Team. 
+                    The information that we request will be retained by us and used as described in this privacy policy.</p>
+
+                <p>The app does use third-party services that may collect information used to identify you.
+                    Link to the privacy policy of third-party service providers used by the app
+                    1. Google Play Services</p>
+                
+                <h2>Log Data</h2>
+                <p>We want to inform you that whenever you use our Service, in a case of an error in the app we collect data 
+                    and information (through third-party products) on your phone called Log Data.</p>
+
+                <p>This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, 
+                    operating system version, the configuration of the app when utilizing our Service, 
+                    the time and date of your use of the Service, and other statistics.</p>
+
+                <h2>Service Providers</h2>
+                <p>We may employ third-party companies and individuals due to the following reasons:</p>
+
+                <p>1. To facilitate our Service;<br>
+                2. To provide the Service on our behalf;<br>
+                3. To perform Service-related services; or <br>
+                4. To assist us in analyzing how our Service is used. <br>
+                5. We want to inform users of this Service that these third parties have access to their Personal Information. <br><br>
+                The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+
+                <h2>Security</h2>
+                <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable 
+                    means of protecting it. But remember that no method of transmission over the internet, 
+                    or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+                
+                <h2>Links to Other Apps</h2>
+                <p>This Service may contain links to other Apps. If you click on a third-party link, 
+                    you will be directed to that site. Note that these external Apps are not operated by us.</p>
+                <p>Therefore, we strongly advise you to review the Privacy Policy of these Apps. 
+                    We have no control over and assume no responsibility for the content, privacy policies, 
+                    or practices of any third-party apps or services.</p>
+
+                <h2>Children’s Privacy</h2>
+                <p>These Services do not address anyone under the age of 13. 
+                    We do not knowingly collect personally identifiable information from children under 13 years of age. 
+                    In the case we discover that a child under 13 has provided us with personal information, 
+                    we immediately delete this from our servers.</p>
+
+                <p>If you are a parent or guardian and you are aware that your child has provided us with personal information,
+                    please contact us so that we will be able to do the necessary actions.</p>
+
+                <h2>Changes to This Privacy Policy</h2>
+                <p>We may update our Privacy Policy from time to time. 
+                    Thus, you are advised to review this page periodically for any changes. 
+                    We will notify you of any changes by posting the new Privacy Policy on this page.<br><br>
+                    This policy is effective as of 2023-07-02</p>
+                
+                <h2>Contact Us</h2>
+                <p>If you have any questions or suggestions about our Privacy Policy,
+                    do not hesitate to contact us at outfitterrr@gmail.com.</p>
+            </div>
+            
+            <h4>©2023, Outfitter., All Rights Reserved</h4>
+        </div>
+    </div>
+</body>
+
+</html>
